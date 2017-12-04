5e0dcc8005aee19b1b2976894e985609a8370672
Sofia Malmsten <Sofia.Malmsten@burohappold.com>
2017-12-04 17:50:28 +0000

    Set up folder structure

---
+++ TAS_Adapter/CRUD/Create.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Base;
+using BHE = BH.oM.Environmental;
+
+namespace TAS_Adapter.CRUD
+{
+    class Create
+    {
+    }
+}
---
+++ TAS_Adapter/TASAdapter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Base;
+using BHE = BH.oM.Environmental;
+using BH.Adapter.Queries;
+
+namespace BH.Adapter.TAS
+{
+    public partial class TASAdapter : BHoMAdapter
+    {
+        /***************************************************/
+        /**** Public fields                             ****/
+        /***************************************************/
+
+
+        /***************************************************/
+        /**** Constructors                              ****/
+        /***************************************************/
+
+    }
+}
