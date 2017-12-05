76726950463e68c2f3cbce3c9778691ea54b9149
Sofia Malmsten <Sofia.Malmsten@burohappold.com>
2017-12-05 15:45:59 +0000

    Added converter - BHoM point to TASPoint

--- TAS_Adapter/Convert/ToTAS.cs
+++ TAS_Adapter/Convert/ToTAS.cs
@@ -4,11 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BH.oM.Base;
+using BHG = BH.oM.Geometry;
 using BHE = BH.oM.Environmental;
+using TAS3D;
+using TBD;
 
 namespace BH.Adapter.TAS
 {
     public static partial class Convert
     {
+        /***************************************/
+        //Object Converters
+        /***************************************/
+            
+
+        /***************************************/
+        //Geometry Converters
+        /***************************************/
+
+        public static TBD.TasPoint FromBHoMGeometry(BHG.Point BHoMPoint)
+        {
+            TBD.TasPoint TASPoint = new TBD.TasPoint();
+            TASPoint.x = (BHoMPoint.X as dynamic);
+            TASPoint.y = (BHoMPoint.Y as dynamic);
+            TASPoint.z = (BHoMPoint.Z as dynamic);
+            return TASPoint;
+        }
+                
+        
+        /***************************************/
+        //Property converter
+        /***************************************/
+
+        /***************************************/
+        //List Converter
+        /***************************************/
+
+
     }
 }
