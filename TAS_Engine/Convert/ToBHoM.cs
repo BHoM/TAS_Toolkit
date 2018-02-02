d23a2b6a1a6c5ac5650995515d634d073962f359
Sofia Malmsten <Sofia.Malmsten@burohappold.com>
2018-02-02 15:07:11 +0000

    Access panels from Spaces

--- TAS_Engine/Convert/ToBHoM.cs
+++ TAS_Engine/Convert/ToBHoM.cs
@@ -38,12 +38,27 @@ namespace BH.Engine.TAS
             BHE.Elements.Space bHoMSpace = new BHE.Elements.Space();
             bHoMSpace.Name = tasZone.name;
 
-            int buildingElementIndex = 1;
+            int buildingElementIndex = 0;
+            int zoneSurfaceIndex = 0;
             while (tasZone.GetSurface(buildingElementIndex) != null)
             {
                 buildingElement tasBuildingElement = tasZone.GetSurface(buildingElementIndex).buildingElement;
                 bHoMSpace.BuildingElements.Add(tasBuildingElement.ToBHoM());
+
+                int roomSrfIndex = 0;
+                while (tasZone.GetSurface(zoneSurfaceIndex).GetRoomSurface(roomSrfIndex) != null)
+                {
+                    zoneSurface tasZoneSrf = tasZone.GetSurface(zoneSurfaceIndex);
+                    RoomSurface tasRoomSrf = tasZoneSrf.GetRoomSurface(roomSrfIndex);
+
+                    //if (tasRoomSrf != null)
+                        bHoMSpace.BuildingElementPanel.Add(tasRoomSrf.ToBHoM());
+
+                    roomSrfIndex++;
+                }
+
                 buildingElementIndex++;
+                zoneSurfaceIndex++;
             }
 
             return bHoMSpace;
@@ -132,7 +147,9 @@ namespace BH.Engine.TAS
             BHE.Elements.BuildingElementPanel bHoMPanel = new BHE.Elements.BuildingElementPanel();
 
             //TBD.RoomSurface currRoomSrf = tasZoneSurface.GetRoomSurface(0); // We need to use all of the room surfaces in the model (Sofia)
+
             TBD.Perimeter currPerimeter = tasRoomSrf.GetPerimeter();
+            if (currPerimeter == null) return bHoMPanel;
             TBD.Polygon currPolygon = currPerimeter.GetFace();
                         
             BHG.Polyline edges = ToBHoM(currPolygon);
