95a56b78ac9f2e93053df39f912642e87b7aacd8
adecler <arnaud.declercq@burohappold.com>
2018-02-25 12:46:25 +0800

    Adapt to changes in Queries

--- TAS_Adapter/TASAdapter.cs
+++ TAS_Adapter/TASAdapter.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using BH.oM.Queries;
+using BH.oM.DataManipulation.Queries;
 using BH.oM.Base;
 
 namespace BH.Adapter.TAS
