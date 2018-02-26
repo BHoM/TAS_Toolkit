6a87cb548a9a947ee01a816acb64114afc0de984
Sofia Malmsten <Sofia.Malmsten@burohappold.com>
2018-02-26 11:49:38 +0000

    Added Query for RGB colours

---
+++ TAS_Engine/Query/GetRGB.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BHG = BH.oM.Geometry;
+using BHEE = BH.oM.Environmental.Elements;
+using BHEI = BH.oM.Environmental.Interface;
+using BH.Engine.Environment;
+
+namespace BH.Engine.TAS
+{
+    public static partial class Query
+    {
+        /***************************************************/
+
+        public static byte [] GetRGB(uint UInt)
+        {
+            byte b = (byte)(UInt >> 16);
+            byte g = (byte)(UInt >> 8);
+            byte r = (byte)(UInt >> 0);
+
+            byte[] RGB = new byte[] { r, g, b };
+
+            return RGB;
+
+        }
+
+        /***************************************************/
+    }
+}
