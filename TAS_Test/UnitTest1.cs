7000fdc50433c6e7d9f689084f733ba9d9368f92
Maria Tjader <maria.tjader@burohappold.com>
2018-10-26 09:39:33 +0100

    Adding Test file for TAS #43

---
+++ TAS_Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TAS_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
