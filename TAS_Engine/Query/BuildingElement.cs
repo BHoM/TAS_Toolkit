2faabaa7f79e179f3096722db6b6a2f3cf8c49eb
Maria Tjader <maria.tjader@burohappold.com>
2018-11-08 15:08:23 +0000

    Start Building elements

--- TAS_Engine/Query/BuildingElement.cs
+++ TAS_Engine/Query/BuildingElement.cs
@@ -7,6 +7,7 @@ using BHG = BH.oM.Geometry;
 using BHEE = BH.oM.Environment.Elements;
 using BHEI = BH.oM.Environment.Interface;
 using BH.Engine.Environment;
+using BH.oM.Geometry;
 
 namespace BH.Engine.TAS
 {
@@ -22,6 +23,20 @@ namespace BH.Engine.TAS
             while ((tbdBuildingElement = tbdBuilding.GetBuildingElement(index)) != null)
             {
                 if (tbdBuildingElement.name == name)
+                    return tbdBuildingElement;
+                index++;
+            }
+
+            return null;
+        }
+
+        public static TBD.buildingElement BuildingElement(TBD.Building tbdBuilding, BH.oM.Geometry.ICurve panelCurve)
+        {
+            int index = 0;
+            TBD.buildingElement tbdBuildingElement = null;
+            while ((tbdBuildingElement = tbdBuilding.GetBuildingElement(index)) != null)
+            {
+                if (tbdBuildingElement. == panelCurve)
                     return tbdBuildingElement;
                 index++;
             }
