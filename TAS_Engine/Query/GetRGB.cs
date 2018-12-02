d4a35308962bdcaf7e16713edab2808b24fdac3f
Michal Dengusiak <michal.dengusiak@burohappold.com>
2018-12-02 18:37:29 +0000

    fix Uvalues

--- TAS_Engine/Query/GetRGB.cs
+++ TAS_Engine/Query/GetRGB.cs
@@ -14,6 +14,13 @@ namespace BH.Engine.TAS
     {
         /***************************************************/
 
+        // From .net/dynamo to a colour set back into Tas
+        //public static uint ColorToUIntRGB(System.Drawing.Color Color)
+        //{
+        //    return (uint)((Color.Blue << 16) | (Color.Green << 8) | (Color.Red << 0));
+        //}
+
+        // From Tas to display in .net/dynamo
         public static System.Drawing.Color GetRGB(uint UInt)
         {
             byte b = (byte)(UInt >> 16);
@@ -22,6 +29,7 @@ namespace BH.Engine.TAS
 
             return System.Drawing.Color.FromArgb(r, b, g);
         }
+
 
         /***************************************************/
     }
