1c1acab011b6ae6e896d9e637f550ecd12a1de8b
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2018-12-18 12:26:51 +0000

    Fix #106 - making user provide info about which result they want

--- TAS_TSDAdapter/CRUD/Read.cs
+++ TAS_TSDAdapter/CRUD/Read.cs
@@ -21,62 +21,68 @@ namespace BH.Adapter.TAS
         /***************************************************/
         /**** Public Methods                            ****/
         /***************************************************/
-        /*
+
         protected override IEnumerable<IBHoMObject> Read(Type type, IList indices = null)
         {
-            if (type == typeof(BH.oM.Environment.Results.SimulationResult))
-                return ReadBuildingResults();
-            //TODO: Impletemt option for HDD and CDD
-            else if (type == typeof(BH.oM.Environment.Results.SimulationResultType))
-
-                return ReadSpaces();
-            //if (type == typeof(BHE.Results.SimulationResult))
-            //    return ReadBuildingResults();
-            else
-                return null;
+            return new List<IBHoMObject>();
         }
 
-        public List<IBHoMObject> Read()
-        {
-            List<IBHoMObject> bhomObjects = new List<IBHoMObject>();
-            bhomObjects.AddRange(ReadBuildingResults());
-            // Add other results, all shall be shown
-            return bhomObjects;
-        }
-
-        /*
-        public List<BH.oM.Environment.Results.SimulationResult> ReadBuildingResults(List<string> ids = null)
-        {
-            TSD.BuildingData tsdBuildingData = tsdDocument.SimulationData.GetBuildingData();
-            TSD.CoolingDesignData tsdCoolingDesignData = tsdDocument.SimulationData.GetCoolingDesignData(0);
-            TSD.HeatingDesignData tsdHeatingDesignData = tsdDocument.SimulationData.GetHeatingDesignData(0);
-
-            List<BH.oM.Environment.Results.SimulationResult> buildingResults = new List<BH.oM.Environment.Results.SimulationResult>();
-            buildingResults.Add(Engine.TAS.Convert.ToBHoMTSDBuilding(tsdBuildingData));
-            List<BH.oM.Environment.Results.SimulationResult> coolingDesignResults = new List<BH.oM.Environment.Results.SimulationResult>();
-            cool.Add(Engine.TAS.Convert.ToBHoMTSDZone(tsdCoolingDesignData));
-            List<BH.oM.Environment.Results.SimulationResult> heatingDesignResults = new List<BH.oM.Environment.Results.SimulationResult>();
-            buildingResults.Add(Engine.TAS.Convert.ToBHoMTSDBuilding(tsdHeatingDesignData));
-            //TODO: Add to ourput 3 set of data CDD and HDD --DONE?
-            
-            return buildingResults;
-        }*/
-        /*
-            //TODO: Space/zone results, ReadSpaceresults similar as ReadBuildingResults
-            public List<BH.oM.Environment.Results.SimulationResult> ReadSpaceResults(List<string> ids = null)
+            /*
+            protected override IEnumerable<IBHoMObject> Read(Type type, IList indices = null)
             {
-                //iterate through all spaces 
-                //TODO: ReadSpaces
+                if (type == typeof(BH.oM.Environment.Results.SimulationResult))
+                    return ReadBuildingResults();
+                //TODO: Impletemt option for HDD and CDD
+                else if (type == typeof(BH.oM.Environment.Results.SimulationResultType))
+
+                    return ReadSpaces();
+                //if (type == typeof(BHE.Results.SimulationResult))
+                //    return ReadBuildingResults();
+                else
+                    return null;
+            }
+
+            public List<IBHoMObject> Read()
+            {
+                List<IBHoMObject> bhomObjects = new List<IBHoMObject>();
+                bhomObjects.AddRange(ReadBuildingResults());
+                // Add other results, all shall be shown
+                return bhomObjects;
+            }
+
+            /*
+            public List<BH.oM.Environment.Results.SimulationResult> ReadBuildingResults(List<string> ids = null)
+            {
                 TSD.BuildingData tsdBuildingData = tsdDocument.SimulationData.GetBuildingData();
                 TSD.CoolingDesignData tsdCoolingDesignData = tsdDocument.SimulationData.GetCoolingDesignData(0);
                 TSD.HeatingDesignData tsdHeatingDesignData = tsdDocument.SimulationData.GetHeatingDesignData(0);
 
                 List<BH.oM.Environment.Results.SimulationResult> buildingResults = new List<BH.oM.Environment.Results.SimulationResult>();
-                buildingResults.Add(Engine.TAS.Convert.ToBHoMTSDZone());
-                //TODO: Add to ourput 3 set of data CDD and HDD
-
+                buildingResults.Add(Engine.TAS.Convert.ToBHoMTSDBuilding(tsdBuildingData));
+                List<BH.oM.Environment.Results.SimulationResult> coolingDesignResults = new List<BH.oM.Environment.Results.SimulationResult>();
+                cool.Add(Engine.TAS.Convert.ToBHoMTSDZone(tsdCoolingDesignData));
+                List<BH.oM.Environment.Results.SimulationResult> heatingDesignResults = new List<BH.oM.Environment.Results.SimulationResult>();
+                buildingResults.Add(Engine.TAS.Convert.ToBHoMTSDBuilding(tsdHeatingDesignData));
+                //TODO: Add to ourput 3 set of data CDD and HDD --DONE?
 
                 return buildingResults;
             }*/
-    }
+            /*
+                //TODO: Space/zone results, ReadSpaceresults similar as ReadBuildingResults
+                public List<BH.oM.Environment.Results.SimulationResult> ReadSpaceResults(List<string> ids = null)
+                {
+                    //iterate through all spaces 
+                    //TODO: ReadSpaces
+                    TSD.BuildingData tsdBuildingData = tsdDocument.SimulationData.GetBuildingData();
+                    TSD.CoolingDesignData tsdCoolingDesignData = tsdDocument.SimulationData.GetCoolingDesignData(0);
+                    TSD.HeatingDesignData tsdHeatingDesignData = tsdDocument.SimulationData.GetHeatingDesignData(0);
+
+                    List<BH.oM.Environment.Results.SimulationResult> buildingResults = new List<BH.oM.Environment.Results.SimulationResult>();
+                    buildingResults.Add(Engine.TAS.Convert.ToBHoMTSDZone());
+                    //TODO: Add to ourput 3 set of data CDD and HDD
+
+
+                    return buildingResults;
+                }*/
+        }
 }
