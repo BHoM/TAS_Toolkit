3be23da24685d7d2c771a4a4508011603bc530b8
adecler <arnaud.declercq@burohappold.com>
2018-12-21 16:29:38 +0800

    Clear the info on assembly company

--- TAS_TBDAdapter/Properties/AssemblyInfo.cs
+++ TAS_TBDAdapter/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("TAS_Adapter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("BuroHappold")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TAS_Adapter")]
 [assembly: AssemblyCopyright("Copyright © https://github.com/BHoM")]
 [assembly: AssemblyTrademark("")]
