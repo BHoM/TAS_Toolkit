0bfa64983eb55ddca0236459d8f2ad6798b69c37
adecler <arnaud.declercq@burohappold.com>
2018-12-21 16:29:38 +0800

    Clear the info on assembly company

--- TAS_oM/Properties/AssemblyInfo.cs
+++ TAS_oM/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("TAS_oM")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("BuroHappold")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TAS_oM")]
 [assembly: AssemblyCopyright("Copyright © https://github.com/BHoM")]
 [assembly: AssemblyTrademark("")]
