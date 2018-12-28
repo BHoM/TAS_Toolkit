c76e9548446454577d150673294fbd9c97abbba5
Maria Tjader <maria.tjader@burohappold.com>
2018-12-28 11:47:45 +0000

    Setting Hour and Day input for Spaces

--- TAS_TSDAdapter/CRUD/Read.cs
+++ TAS_TSDAdapter/CRUD/Read.cs
@@ -88,7 +88,7 @@ namespace BH.Adapter.TAS
                 TSD.SurfaceData srfData = null;
                 while((srfData = zoneData.GetSurfaceData(srfIndex)) != null)
                 {
-                    buildingElementResults.Add(Engine.TAS.Convert.ToBHoMTSDSurface(srfData, ProfileResultUnits, ProfileResultType));
+                    buildingElementResults.Add(Engine.TAS.Convert.ToBHoMTSDSurface(srfData, ProfileResultUnits, ProfileResultType, Hour, Day));
                     srfIndex++;
                 }
                 zoneIndex++;
