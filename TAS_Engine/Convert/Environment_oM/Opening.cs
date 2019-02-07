4c76cbbb01ff1ab7f4443948e6880d8c46f18ff4
michaldengusiak <michal.dengusiak@burohappold.com>
2019-02-07 10:49:43 +0000

    Some construction pulling start

--- TAS_Engine/Convert/Environment_oM/Opening.cs
+++ TAS_Engine/Convert/Environment_oM/Opening.cs
@@ -41,11 +41,118 @@ namespace BH.Engine.TAS
         [Description("BH.Engine.TAS.Convert ToBHoM => gets a BHoM Environmental Opening from a TAS TBD Opening Polygon")]
         [Input("tbdBuilding", "TAS TBD Building")]
         [Output("BHoM Environmental Opening")]
-        public static BHE.Opening ToBHoMOpening(this TBD.Polygon tbdPolygon)
+        public static BHE.Opening ToBHoMOpening(this TBD.Polygon tbdPolygon, TBD.RoomSurface roomSurface)
         {
             BHE.Opening opening = new oM.Environment.Elements.Opening();
+            TBD.zoneSurface tbdSurface = roomSurface.zoneSurface;
+            TBD.buildingElement tbdElement = tbdSurface.buildingElement;
 
-            opening.OpeningCurve = tbdPolygon.ToBHoM();
+            if (roomSurface != null)
+
+            {
+
+                //opening.CustomData.Add("TAS_ElementID", roomSurface.parentSurface.zoneSurface.GUID);
+                opening.CustomData.Add("TAS_ElementID", roomSurface.zoneSurface.GUID);
+                //SL will not work
+                //opening.CustomData.Add("TAS_ParentBuildingElementName", roomSurface.parentSurface.zoneSurface.buildingElement.name);
+                opening.CustomData.Add("TAS_BuildingElementName", roomSurface.zoneSurface.buildingElement.name);
+
+                //Get Constructions for Opening
+                TBD.buildingElement bE = roomSurface.zoneSurface.buildingElement;
+                TBD.Construction Constuction = null;
+                Constuction = bE.GetConstruction();
+                string aName = Constuction.name;
+
+                //Adding data to Extended Poroperties
+                //BH.oM.Environment.Properties.ElementProperties elementProperties = new oM.Environment.Properties.ElementProperties();
+                //elementProperties.Construction = Constuction.ToBHoM();
+                //elementProperties.BuildingElementType = ((TBD.BuildingElementType)bE.BEType).ToBHoM().FixType(bE, roomSurface.zoneSurface);
+                //opening.ExtendedProperties.Add(elementProperties);
+
+                //If we have Frame we get polygon otherwise for pane we get perimeter
+                try
+                {
+
+                    if (tbdPolygon != null)
+                    {
+
+                        opening.OpeningCurve = tbdPolygon.ToBHoM();
+
+                    }
+                    else
+                    {
+                        opening.OpeningCurve = roomSurface.GetPerimeter().ToBHoM();
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    BH.Engine.Reflection.Compute.RecordError(ex.ToString());
+                    opening.OpeningCurve = null;
+                }
+
+
+            }
+
+            try
+            {
+
+                if (roomSurface.parentSurface.zoneSurface.GUID != null)
+                {
+                    opening.CustomData.Add("TAS_ParentBuildingElementName", roomSurface.parentSurface.zoneSurface.buildingElement.name);
+
+                }
+            }
+            catch 
+            {
+
+            }
+
+
+            //Adding data to Extended Poroperties--------------------------------------------------------------------------------------------------------------
+
+            //EnvironmentContextProperties
+            BH.oM.Environment.Properties.EnvironmentContextProperties environmentContextProperties = new oM.Environment.Properties.EnvironmentContextProperties();
+            environmentContextProperties.ElementID = tbdSurface.GUID;
+            environmentContextProperties.Description = tbdSurface.buildingElement.name + " - " + tbdSurface.buildingElement.GUID;
+            environmentContextProperties.TypeName = tbdSurface.buildingElement.name;
+            opening.ExtendedProperties.Add(environmentContextProperties);
+
+            //BuildingElementContextProperties
+            BH.oM.Environment.Properties.BuildingElementContextProperties buildingElementContextProperties = new oM.Environment.Properties.BuildingElementContextProperties();
+            buildingElementContextProperties.ConnectedSpaces.Add(tbdSurface.zone.name);
+            if ((int)tbdSurface.type == 3)
+                buildingElementContextProperties.ConnectedSpaces.Add(tbdSurface.linkSurface.zone.name);
+            else
+                buildingElementContextProperties.ConnectedSpaces.Add("-1");
+
+            buildingElementContextProperties.IsAir = tbdElement.ghost != 0;
+            buildingElementContextProperties.IsGround = tbdElement.ground != 0;
+            buildingElementContextProperties.Colour = BH.Engine.TAS.Query.GetRGB(tbdElement.colour).ToString(); //we need to fix Colot type to 'System.Drawing.Color' type currently transfrom to Int
+            buildingElementContextProperties.Reversed = tbdSurface.reversed != 0;
+            opening.ExtendedProperties.Add(buildingElementContextProperties);
+
+            //BuildingElementAnalyticalProperties
+            BH.oM.Environment.Properties.BuildingElementAnalyticalProperties buildingElementAnalyticalProperties = new oM.Environment.Properties.BuildingElementAnalyticalProperties();
+            buildingElementAnalyticalProperties.Altitude = tbdSurface.altitude;
+            buildingElementAnalyticalProperties.AltitudeRange = tbdSurface.altitudeRange;
+            buildingElementAnalyticalProperties.Inclination = tbdSurface.inclination;
+            buildingElementAnalyticalProperties.Orientation = tbdSurface.orientation;
+            buildingElementAnalyticalProperties.GValue = tbdElement.GValue();
+            buildingElementAnalyticalProperties.LTValue = tbdElement.LTValue();
+            buildingElementAnalyticalProperties.UValue = tbdElement.UValue();
+            opening.ExtendedProperties.Add(buildingElementAnalyticalProperties);
+
+            //ElementProperties
+            BH.oM.Environment.Properties.ElementProperties elementProperties = new oM.Environment.Properties.ElementProperties();
+            elementProperties.BuildingElementType = ((TBD.BuildingElementType)tbdElement.BEType).ToBHoM().FixType(tbdElement, tbdSurface);
+            elementProperties.Construction = tbdElement.GetConstruction().ToBHoM();
+            opening.ExtendedProperties.Add(elementProperties);
+
+
+            //Extended Poroperties-------------------------------------------------------------------------------------------------------------------------
+
+
             return opening;
         }
 
