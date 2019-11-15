d85d53e70bc38f7bc3f3bfcc3ffe6d9bf45b96a3
Maja Lindroth <maja.lindroth@burohappold.com>
2019-11-15 12:24:59 +0000

    #221 Added TASSettings to TAS oM and TAS Engine create

---
+++ TAS_Engine/Create/TASSettings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BH.oM.TAS.Settings;
+
+using BH.oM.Reflection.Attributes;
+using System.ComponentModel;
+
+namespace BH.Engine.TAS
+{
+    public static partial class Create
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+        [Description("")]
+        [Input("distanceTolerance", "distanceTolerance is used as input for CleanPolyline method used for opening, default is set to BH.oM.Geometry.Tolerance.Distance")]
+        [Input("planarTolerance", "Set tolerance for planar surfaces, default is set to ")]
+        public static TASSettings TASSettings(double planarTolerance = BH.oM.Geometry.Tolerance.Distance, double DistanceTolerance = BH.oM.Geometry.Tolerance.Distance)
+        {
+            return new TASSettings
+            {
+                PlanarTolerance = planarTolerance,
+                DistanceTolerance = DistanceTolerance,
+            };
+        }
+    }
+}
