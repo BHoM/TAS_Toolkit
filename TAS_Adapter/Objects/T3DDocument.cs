c855ae053f945f008b10192c653448b960a0ad18
Thomas Edward Kingstone <Tom.Kingstone@burohappold.com>
2024-01-08 10:09:17 +0000

    made instantiation of T3D and TSD documents null, and passing in a document when instantiating

--- TAS_Adapter/Objects/T3DDocument.cs
+++ TAS_Adapter/Objects/T3DDocument.cs
@@ -8,7 +8,7 @@ namespace BH.Adapter.TAS
 {
     public class T3DDocument : ITASFile
     {
-        public virtual TAS3D.T3DDocument Document { get; set; } = new TAS3D.T3DDocument();
+        public virtual TAS3D.T3DDocument Document { get; set; } = null;
 
         public virtual string FilePath { get; set; } = "";
     }
